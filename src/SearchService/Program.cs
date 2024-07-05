554ab967d5a479a83967742d9a4b9188639652a8
Richard Emijere <emijere.richard@gmail.com>
2024-07-05 11:14:07 +0100

    cleanup and database setup

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,36 +1,37 @@
+using MongoDB.Driver;
+using MongoDB.Entities;
+using SearchService.Models;
 
-namespace SearchService
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+var mongoClientSettings = MongoClientSettings.FromConnectionString(builder.Configuration.GetConnectionString("MongoDbConnection"));
+builder.Services.AddSingleton<IMongoClient>(new MongoClient(mongoClientSettings));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+try
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
+    await DB.InitAsync("SearchDb", mongoClientSettings);
 
-            // Add services to the container.
-
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
-
-            app.MapControllers();
-
-            app.Run();
-        }
-    }
+    await DB.Index<Item>()
+        .Key(x => x.Make, KeyType.Text)
+        .Key(x => x.Model, KeyType.Text)
+        .Key(x => x.Color, KeyType.Text)
+        .CreateAsync();
 }
+catch (Exception e)
+{
+    Console.WriteLine(e);
+}
+
+app.Run();
