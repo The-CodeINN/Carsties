3cb793d8d0268cc38b2da28ec9d8e4b9b347d8f6
Richard Emijere <emijere.richard@gmail.com>
2024-08-02 11:51:50 +0100

    done with consumer config.

---
+++ src/AuctionService/Consumers/AuctionFinishedConsumer.cs
@@ -0,0 +1,27 @@
+﻿using AuctionService.Data;
+using Contracts;
+using MassTransit;
+
+namespace AuctionService.Consumers;
+
+public class AuctionFinishedConsumer(AuctionDbContext dbContext) : IConsumer<AuctionFinished>
+{
+    private readonly AuctionDbContext _dbContext = dbContext;
+
+    public async Task Consume(ConsumeContext<AuctionFinished> context)
+    {
+        Console.WriteLine("---- Consuming AuctionFinished ----");
+
+        var auction = await _dbContext.Auctions.FindAsync(context.Message.AuctionId);
+
+        if (context.Message.ItemSold)
+        {
+            auction.Winner = context.Message.Winner;
+            auction.SoldAmount = context.Message.Amount;
+        }
+
+        auction.Status = auction.SoldAmount > auction.ReservePrice ? Entities.Status.Finished : Entities.Status.ReserveNotMet;
+
+        await _dbContext.SaveChangesAsync();
+    }
+}
